12142cdb305f8981bbd6393becb5a738df7d3e42
Kriogenik <thkriogenik@gmail.com>
2020-08-10 21:58:20 +0300

    Initial commit

---
+++ src/test/java/com/thekriogenik/springdata/SpringDataDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thekriogenik.springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
